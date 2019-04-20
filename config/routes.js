f33df33b9154bb91c40ea980a86b0262161e0251
thomasutsey <tomutsey@yahoo.com>
2019-04-20 16:43:22 -0400

    Mvp for sprint

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
-
+const db = require('../config/model')
+const crypt = require('bcryptjs')
+const jwtKey = require('../auth/authenticate')
 const { authenticate } = require('../auth/authenticate');
+const jwt = require("jsonwebtoken")
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +13,42 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+    let user = req.body;
+    const hash = crypt.hashSync( user.password,10);
+    user.password=hash;
+    console.log(user)
+    db.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+        .catch(error => {res.status(500).json(error)});
 }
 
+
+
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+  // console.log (username)
+  
+  
+  db.findBy({ username})
+  
+    .first()
+    .then(user => {  
+      
+      if (user && crypt.compareSync(password,user.password)) {
+        console.log(user)
+       const token = generateToken(user)
+        // console.log(token)
+        res.status(200).json({ message: `Welcome ${user}!}), have a token`,token,});
+      } else {
+        
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -29,4 +64,14 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    user:user.username,
+  }
+  const options = {
+    expiresIn:'1d'
+  }
+  return jwt.sign(payload,'secret MESSAGE',options);
 }
